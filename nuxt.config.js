c89d40287c3f759cac6faf6f25d05cd1ee696b12
Alexandr Stepanov <742the@gmail.com>
2020-05-07 11:30:14 +0300

    ui + header + 1 block + fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  { rel: 'stylesheet', href: '/Fonts/fonts.css'}],
   },
   /*
    ** Customize the progress-bar color
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <Header/>
+    <Cover/>
+    <Video/>
+  </div>
 </template>
 
-<script></script>
+<script>
+  import Header from '@/components/Header';
+  import Cover from '@/components/Cover';
+  import Video from '@/components/Video';
+  export default {
+    components: {
+      Header,
+      Cover,
+      Video,
+    }
+  };
+
+</script>
 
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
 }
 </style>
