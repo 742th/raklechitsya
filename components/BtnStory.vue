800b1ed6b1e27c2d8835746188a22eb2008656ae
Metanka <61090329+Metanka@users.noreply.github.com>
2020-05-08 17:27:05 +0300

    Сверстала блок Истории неизлечимых привычек без JS (#2)
    * Сверстала блок Истории неизлечимых привычек без JS
    
    * Карточки сделала ссылками

---
+++ components/BtnStory.vue
@@ -0,0 +1,33 @@
+.<template>
+    <button class="BtnStory">
+      <nuxt-link to="/" class="BtnStory__link">Больше статей</nuxt-link>
+      </button>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .BtnStory {
+    width: calc(100vw - 60px*2);
+    height: 82px;
+    background: #FBFBFB;
+    border: none;
+    font-size: 16px;
+    line-height: 20px;
+  }
+
+  .BtnStory:hover {
+    background: #F8F8F8;
+    cursor: pointer;
+  }
+
+  .BtnStory__link {
+    text-decoration: none;
+    color: black;
+  }
+  
+</style>
