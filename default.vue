14c6e629720e7a535809cbd39889fa1521c27642
Alexey Roschektaev <55943475+roschek@users.noreply.github.com>
2020-05-09 21:02:35 +0300

    Add files via upload

---
+++ default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: Inter, Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
