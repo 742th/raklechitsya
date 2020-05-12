36ca35243b447db986b771a081faf225aed87f9c
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-05-12 22:18:57 +0400

    Почти готовая страница ShowStory, осталось импортировать и подстилизовать кнопки

--- components/blocks/FullStory.vue
+++ components/blocks/FullStory.vue
@@ -1,64 +1,68 @@
 <template>
-  <section class="story">
-    <div class="story__container">
-      <span class="story__photo-container">
-        <img
-          src="https://www.sportsdaily.ru/s/image/92188.jpg"
-          alt=""
-          class="story__photo"
-        />
-      </span>
-      <div class="story__brief-container">
-        <h1 class="story__title">
-          <span>Александр Тарханов:</span>
-          «Я не могу победить свою пунктуальность в отличии от рака»
-        </h1>
-        <div class="story__footer">
-          <button class="story__button_brief">Поделитесь ↗</button>
-          <p class="story__date">20 апреля 2018</p>
+  <panel>
+    <section class="story">
+      <div class="story__container">
+        <span class="story__photo-container">
+          <img
+            src="https://www.sportsdaily.ru/s/image/92188.jpg"
+            alt=""
+            class="story__photo"
+          />
+        </span>
+        <div class="story__brief-container">
+          <h1 class="story__title">
+            <span>Александр Тарханов:</span>
+            «Я не могу победить свою пунктуальность в отличии от рака»
+          </h1>
+          <div class="story__footer">
+            <button class="story__button_brief">Поделитесь ↗</button>
+            <p class="story__date">20 апреля 2018</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="content__container">
-      <p class="story__text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, beatae
-        hic inventore voluptates aliquam officia voluptas recusandae? Nihil
-        perspiciatis veritatis odio iusto! Odio quo, vel sapiente eaque est,
-        autem laborum ab quibusdam accusantium veritatis illum omnis facilis
-        ducimus facere numquam neque odit unde! Est aut ex consequatur ad nihil
-        facilis impedit voluptate totam, velit temporibus expedita consectetur
-        molestias nam iste accusantium neque corporis illum non sunt modi
-        suscipit, quam, quia nulla. Numquam, rem? Blanditiis aperiam adipisci
-        nisi, eaque nulla, eveniet nam labore illum asperiores atque maxime
-        aspernatur. Natus, aliquam sed aperiam eum nihil, explicabo distinctio
-        cupiditate delectus eveniet laboriosam nemo! Necessitatibus tempora
-        ipsam quis libero modi alias, dignissimos corporis, totam iste error,
-        laudantium eius reiciendis! Distinctio, quia. Quas tempore commodi
-        excepturi veniam dolore. Inventore hic explicabo sint tempore iusto sunt
-        magni molestias, optio quo eos enim sapiente sed, itaque quod eum
-        deleniti, veritatis libero debitis mollitia nemo vel cum eius repellat.
-        Obcaecati modi aliquid, assumenda natus minima sapiente placeat atque
-        cupiditate nostrum nulla quis odit molestiae sint a repudiandae sit?
-        Incidunt non dolores consequuntur et aut nobis quia. Earum saepe et,
-        dolorem odit quas, distinctio ullam optio at ea laudantium sit impedit
-        dolor doloremque reprehenderit nesciunt, id soluta ipsam mollitia.
-      </p>
-      <button class="story__button">
-        Поделитесь этой статьей в своих социальных сетях ↗
-      </button>
-    </div>
-  </section>
+      <div class="content__container">
+        <p class="story__text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum,
+          beatae hic inventore voluptates aliquam officia voluptas recusandae?
+          Nihil perspiciatis veritatis odio iusto! Odio quo, vel sapiente eaque
+          est, autem laborum ab quibusdam accusantium veritatis illum omnis
+          facilis ducimus facere numquam neque odit unde! Est aut ex consequatur
+          ad nihil facilis impedit voluptate totam, velit temporibus expedita
+          consectetur molestias nam iste accusantium neque corporis illum non
+          sunt modi suscipit, quam, quia nulla. Numquam, rem? Blanditiis aperiam
+          adipisci nisi, eaque nulla, eveniet nam labore illum asperiores atque
+          maxime aspernatur. Natus, aliquam sed aperiam eum nihil, explicabo
+          distinctio cupiditate delectus eveniet laboriosam nemo! Necessitatibus
+          tempora ipsam quis libero modi alias, dignissimos corporis, totam iste
+          error, laudantium eius reiciendis! Distinctio, quia. Quas tempore
+          commodi excepturi veniam dolore. Inventore hic explicabo sint tempore
+          iusto sunt magni molestias, optio quo eos enim sapiente sed, itaque
+          quod eum deleniti, veritatis libero debitis mollitia nemo vel cum eius
+          repellat. Obcaecati modi aliquid, assumenda natus minima sapiente
+          placeat atque cupiditate nostrum nulla quis odit molestiae sint a
+          repudiandae sit? Incidunt non dolores consequuntur et aut nobis quia.
+          Earum saepe et, dolorem odit quas, distinctio ullam optio at ea
+          laudantium sit impedit dolor doloremque reprehenderit nesciunt, id
+          soluta ipsam mollitia.
+        </p>
+        <button class="story__button">
+          Поделитесь этой статьей в своих социальных сетях ↗
+        </button>
+      </div>
+    </section>
+  </panel>
 </template>
 
 <script>
-export default {};
+import Panel from '@/components/Panel';
+export default {
+  components: {
+    panel: Panel,
+  },
+};
 </script>
 
 <style scoped>
-.story {
-  padding-top: 100px;
-}
-
 .story__container {
   flex-shrink: 0;
   display: flex;
@@ -98,6 +102,7 @@ export default {};
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 130px 0 160px 0;
 }
 
 .story__title {
@@ -123,6 +128,6 @@ export default {};
 .story__text {
   font-size: 22px;
   line-height: 1.363;
-  padding: 130px 0 70px 0;
+  padding-bottom: 70px;
 }
 </style>
--- pages/ShowStory.vue
+++ pages/ShowStory.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="container">
+    <full-story />
     <panel>
-      <full-story />
+      <section class="stories-cards">
+        <div class="stories-cards__container">
+          <card />
+          <card />
+          <card />
+          <card />
+        </div>
+        <button class="stories-cards__button">Больше статей</button>
+      </section>
     </panel>
   </div>
 </template>
 
 <script>
+import Card from '@/components/Card';
 import Panel from '@/components/Panel';
 import FullStory from '@/components/blocks/FullStory';
 export default {
   components: {
     'full-story': FullStory,
     panel: Panel,
+    card: Card,
   },
 };
 </script>
 
 <style scoped>
 .container {
-  max-width: 1440px;
+  padding: 100px 0;
+}
+.stories-cards {
+  text-align: center;
+}
+
+.stories-cards__container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 40px;
+  padding-bottom: 70px;
 }
 </style>
